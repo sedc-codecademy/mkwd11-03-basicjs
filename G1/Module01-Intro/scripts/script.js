5d8212f37be38f2a89b261471003b9d09a52a321
Bojan Damchevski <bod@proofficepark.dk>
2022-12-05 20:48:23 +0100

    Class01 code

---
+++ G1/Module01-Intro/scripts/script.js
@@ -0,0 +1,72 @@
+// alert("Hello from Visual Studio Code");
+console.log("Hello from Javascript");
+
+// This is undefined
+var firstName;
+
+// This is a string
+var lastName = "Damchevski";
+
+console.log(firstName, lastName);
+
+// This is a number
+let firstNumber = 1;
+
+// This is also a string
+let age = "24";
+
+// This is a number
+let numberAge = 24;
+
+// This is a number
+let decimalNumber = 2.5;
+
+// This variable is not defined, that's why it's undefined
+let undefinedValue;
+console.log(undefinedValue);
+
+// This is a boolean
+let knowsHtml = true;
+let knowJS = false;
+
+// This is a null
+let bojan = null;
+console.log(bojan);
+
+console.log("===================================================");
+
+let stringExample = "Tosho";
+let numberExample = 100;
+let booleanExample = true;
+let undefinedExample;
+let nullExample = null;
+console.log(typeof stringExample);
+console.log(typeof numberExample);
+console.log(typeof booleanExample);
+console.log(typeof undefinedExample);
+console.log(typeof nullExample);
+
+let stringNumber = "23123";
+console.log(typeof stringNumber);
+
+let numberA = 20;
+let numberB = "20";
+
+console.log(numberA == numberB);
+console.log(numberA === numberB);
+
+console.log("HELLO FROM \n BOJANCO");
+
+/*This is
+a
+multiline
+comment
+*/
+
+let someVariable = "Bojan";
+
+someVariable = "Ivan";
+
+someVariable = 23;
+
+someVariable = false;
