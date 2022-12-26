1d1071e566fde2eceee593765652a9e76ae81314
Manass2 <panemanass2@gmail.com>
2022-12-26 21:01:29 +0100

    class 9 update

--- G5/class09/class-code/script.js
+++ G5/class09/class-code/script.js
@@ -26,5 +26,35 @@ $(document).ready(function() {
     console.log(firstChild);
     console.log(secondChild);
     console.log(lastChild);
+    console.log(firstChild.text());
 
+    let inputValue = $('input').first().val();
+    console.log(inputValue);
+
+    changedInputValue = $('input').first().val('something');
+    // console.log(changedInputValue);
+
+    // getting and setting html content with jquery
+    $('.emptyDiv').first().html('<p>I am a paragraph generarate from Jquery</p>');
+    console.log($('.emptyDiv').first().html())
+
+    // adding content before and after an element
+    $('h1').first().before('<p>New p tag before the h1</p>');
+    $('h1').first().after('<p>New p tag after the h1</p>');
+
+    // changing style with jquery
+    elementsByClassName.first().css('color', 'blue');
+    elementsByClassName.last().css({'color': 'red','font-weight': 'bold', 'background-color': 'green'});
+
+    // Events in Jquery
+
+    let button = $('#btn').first();
+
+    // button.on('click', function() {
+    //     alert('Hello from Jquery');
+    // })
+
+    button.click(function() {
+        alert('I am the simplest event handler');
+    });
 })
